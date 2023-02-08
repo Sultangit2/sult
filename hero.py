7359268e026943aa306aad32643cb3777682e50e
sultangit2000 <tassboke@gmail.com>
2023-02-08 23:08:26 +0600

    'comm'

--- hero.py
+++ hero.py
@@ -26,3 +26,69 @@ hero.display_name()
 hero.multiply_health()
 print(hero)
 print("Length of catchphrase:", len(hero))
+
+
+
+
+class AirHero(SuperHero):
+    people = 'people'
+
+    def __init__(self, name, nickname, superpower, health_points, catchphrase, damage=False, fly=False):
+        super().__init__(name, nickname, superpower, health_points, catchphrase)
+        self.damage = damage
+        self.fly = fly
+    def health_2(self):
+        self.fly = True
+        self.health_points **= 2
+        print(f"Updated health points: {self.health_points}")
+
+    def method_fly(self):
+        print(f'Fly in the {self.fly}_phrase')
+
+class CosmicHero(SuperHero):
+    people = 'people'
+
+    def __init__(self, name, nickname, superpower, health_points, catchphrase, damage=False, fly=False):
+        super().__init__(name, nickname, superpower, health_points, catchphrase)
+        self.damage = damage
+        self.fly = fly
+
+
+    def health_2(self):
+        self.fly = True
+        self.health_points **= 2
+        print(f"Updated health points: {self.health_points}")
+
+    def method_fly(self):
+        print(f'Fly in the {self.fly}_phrase')
+
+
+airHero = AirHero('Tatsumaki', 'Tornado', 'Esper', 200, 'Know your place, B-Class!', 100)
+print(airHero)
+airHero.health_2()
+airHero.method_fly()
+print()
+
+cosmicHero = CosmicHero('King', 'Kingu', 'Strong Heartbeats', 80, 'Im the strongest man on earth. The S-Class Rank 7 /'
+                                                                  'hero, King. Do you know that!?!', 150)
+print(cosmicHero)
+cosmicHero.health_2()
+cosmicHero.method_fly()
+print()
+
+class Villain(CosmicHero):
+
+    def __init__(self, name, nickname, superpower, health_points, catchphrase):
+        super().__init__(name, nickname, superpower, health_points, catchphrase)
+        SuperHero.people = 'monster'
+
+    def gen_X(self):
+        pass
+
+    def crit(self, hero):
+        return hero.damage ** 2
+
+
+villain = Villain('Psykos', 'Saikosu', 'esper', 1000, 'You lost your chance at winning./'
+                ' I was chosen as a superior entity. And I was bestowed more power than you possess.')
+print(villain.crit(airHero))
